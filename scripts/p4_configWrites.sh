9cbe6205757c50ff552d78203b5f5ee182fc910c
mateussaquetti <mateussaquetti@hotmail.com>
2019-12-07 18:58:22 -0300

    A simple update to add a blank line after print the config_writes

--- scripts/p4_configWrites.sh
+++ scripts/p4_configWrites.sh
@@ -33,5 +33,6 @@
 
 cd ${P4_PROJECT_DIR}/config_writes/ && cat confW_* >> config_writes.txt
 cd ${P4_PROJECT_DIR}/config_writes/ && cat config_writes.txt
+echo ""
 cd ${P4_PROJECT_DIR} && make gen_config_writes
 cd ${NF_DESIGN_DIR}/test/sim_switch_default && make
